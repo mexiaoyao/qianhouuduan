b4c5781c72d000d744db4e3e88cdaf425226c545
zhenglei <mexiaoyao@163.com>
2023-10-07 21:20:12 +0800

    定时器

---
+++ times/src/test/java/com/times/TimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.times;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
