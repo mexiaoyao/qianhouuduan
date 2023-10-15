048c0d455b853c8d2e6c6cc242742bdfb2e44a59
zhenglei <mexiaoyao@163.com>
2023-10-15 21:42:51 +0800

    00

--- times/src/main/java/com/times/util/QuartzManagerUtil.java
+++ times/src/main/java/com/times/util/QuartzManagerUtil.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
