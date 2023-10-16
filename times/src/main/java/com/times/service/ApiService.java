9e3ab2cf1730f70dd0e1c817004c95e11456717a
zhenglei <mexiaoyao@163.com>
2023-10-16 22:44:11 +0800

    11

--- times/src/main/java/com/times/service/ApiService.java
+++ times/src/main/java/com/times/service/ApiService.java
@@ -3,6 +3,9 @@ package com.times.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.times.model.Api;
 
+/**
+ * @author 17518
+ */
 public interface ApiService extends IService<Api> {
 
     Integer apiDelete(String id);
--- times/src/test/java/com/times/TimesApplicationTests.java
+++ times/src/test/java/com/times/TimesApplicationTests.java
@@ -1,10 +1,7 @@
 package com.times;
 
-import com.times.service.ApiService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class TimesApplicationTests {
